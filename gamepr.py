2a404c5033d66b7e4c3ac3068d74737e96280cc9
nastya1219 <113636631+nastya1219@users.noreply.github.com>
2023-04-05 09:35:32 +0300

    Add files via upload

---
+++ gamepr.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1099, 668))
+pygame.display.set_caption("proga")
+icon = pygame.image.load('images/Ghost-64.webp')
+pygame.display.set_icon(icon)
+
+
+bg = pygame.image.load('images/bg.png')
+
+bg_x = 0
+
+running = True
+while running:
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x + 1099, 0))
+
+    bg_x -= 0.5
+    if bg_x == -1099:
+        bg_x = 0
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
+
+
