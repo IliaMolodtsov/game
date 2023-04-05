8356eda12714f0fde36451c638974479f77d5f3c
nastya1219 <113636631+nastya1219@users.noreply.github.com>
2023-04-06 00:44:07 +0300

    Add files via upload

--- gamepr.py
+++ gamepr.py
@@ -1,13 +1,13 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1099, 668))
+screen = pygame.display.set_mode((1099, 669))
 pygame.display.set_caption("proga")
 icon = pygame.image.load('images/Ghost-64.webp')
 pygame.display.set_icon(icon)
 
 
-bg = pygame.image.load('images/bg.png')
+bg = pygame.image.load('images/bg5.png')
 
 bg_x = 0
 
@@ -16,7 +16,7 @@ while running:
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 1099, 0))
 
-    bg_x -= 0.5
+    bg_x -= 1
     if bg_x == -1099:
         bg_x = 0
 
